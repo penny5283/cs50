670936a5b2615949823f64764d0cf834fbdd9b4f
penny5283 <74210456+penny5283@users.noreply.github.com>
2023-09-12 06:20:18 +0000

    Tue, Sep 12, 2023, 6:20 AM +00:00

--- population/population.c
+++ population/population.c
@@ -3,8 +3,25 @@
 
 int main(void)
 {
+    int get_start_size(void)
+    {
+        int n;
+        do {
+            n = get_int("Start size")
+        }
+        while (n > 9);
+        return n
+    }
     // TODO: Prompt for start size
-
+    int get_start_size(void)
+        {
+            int n2;
+            do {
+                n2 = get_int("Start size")
+            }
+            while (n2 > n);
+            return n2
+        }
     // TODO: Prompt for end size
 
     // TODO: Calculate number of years until we reach threshold
