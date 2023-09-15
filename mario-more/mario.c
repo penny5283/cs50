9434fdafbf26cbde1dc8a0c6a8909636b4a3ecff
penny5283 <74210456+penny5283@users.noreply.github.com>
2023-09-15 16:38:17 +0000

    Fri, Sep 15, 2023, 9:38 AM -07:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,7 +3,8 @@
 
 int main(void)
 {
-    int n = get_int("n = ?\n")
+    int n = get_int("Height: \n")
+    while( n < 1) or (n > 8); 
     for (int i = n; i <= 8; i++)
         {
             for (int j = 8 - n; j)
