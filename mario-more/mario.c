a5481869b3c0e59721b70a989ccb41ee3d7045d7
penny5283 <74210456+penny5283@users.noreply.github.com>
2023-09-15 16:56:40 +0000

    Fri, Sep 15, 2023, 9:56 AM -07:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,13 +3,13 @@
 
 int main(void)
 {
-
+    int n;
     do
-    int n = get_int("Height: \n")
-    while( n < 1) or (n > 8);
-    for (int i = n; i <= 8; i++)
+        n = get_int("Height: \n")
+    while( n < 1||n > 8);
+    for (int i = 0; i < n; i++)
         {
-            for (int j = n; j)
+            for (int j = 0; j<)
             {
                 printf(" " * (8-n))
                 printf("#")
