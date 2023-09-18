287f7b5f956a1f4b17631202cc8e979252847029
penny5283 <74210456+penny5283@users.noreply.github.com>
2023-09-18 00:51:27 +0000

    Sun, Sep 17, 2023, 5:51 PM -07:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -11,7 +11,7 @@ int main(void)
         {
             for (int j = 1; j<= n; j++ )
             {
-                printf(" " * (8-n)+ n*"#"+2*" "+n*"#");
+                printf("1");
             }
             print("\n");
         }
