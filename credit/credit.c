846a4ccfaee388e6b7f015e8711f6a5dc08ac9e0
penny5283 <74210456+penny5283@users.noreply.github.com>
2023-09-19 03:51:41 +0000

    Mon, Sep 18, 2023, 8:51 PM -07:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,49 +3,62 @@
 
 int main(void)
 {
-    // prompte user for a credit number
+    // Prompt user for a credit number
     long long card_number;
     do
     {
-    card_number = get_long_long("Credit card number: ");
+        card_number = get_long_long("Credit card number: ");
     }
     while (card_number <= 0);
 
     int sum = 0;
     int digit_count = 0;
-    int res = 0;
+    int first_digit = 0; // Initialize first_digit
     int digit;
 
+    // Calculate the sum of the credit card digits
     while (card_number > 0)
     {
-        int digit = card_number % 10;
+        digit = card_number % 10;
         card_number /= 10;
         digit_count++;
-        first_digit = card_number;
-
         if (digit_count % 2 == 0)
         {
             digit *= 2;
-        }
             if (digit > 9)
             {
                 digit -= 9;
             }
+        }
         sum += digit;
     }
 
-
-// check if the card number is valid and determine its type
-if(sum % 10 == 0)
-{
-    if(digit_count == 15 && (card_number ==34 || card_number == 37))
+    // Check if the card number is valid and determine its type
+    if (sum % 10 == 0)
     {
-        printf("AMEX\n");
+        card_number = card_number * 10 + digit; // Restore card_number to its original value
+        if ((digit_count == 15) && ((card_number == 34) || (card_number == 37)))
+        {
+            printf("AMEX\n");
+        }
+        else if ((digit_count == 16) && (card_number >= 51 && card_number <= 55))
+        {
+            printf("MASTERCARD\n");
+        }
+        else if ((digit_count == 13 || digit_count == 16) && (first_digit == 4))
+        {
+            printf("VISA\n"); // Changed "CREDIT" to "VISA"
+        }
+        else
+        {
+            printf("INVALID\n"); // Added missing '\n' and semicolon
+        }
     }
-    else if(digit_count == 16 && (card_number >= 51 && card_number <= 55))
-        printf("MASTERCARD\n");
-    else if((digit_count == 13 || digit_count ==16)) && (first_digit == 4)
-        printf("CREDIT\n")
     else
-        printf("INVALID")
+    {
+        printf("INVALID\n");
+    }
+
+    return 0;
 }
+
