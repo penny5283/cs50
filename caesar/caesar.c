d50f0dd88fff485951c58c91e3eb8ac66f6a1703
penny5283 <74210456+penny5283@users.noreply.github.com>
2023-10-03 03:44:55 +0000

    Mon, Oct 2, 2023, 8:44 PM -07:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -7,8 +7,8 @@ int main(int argc, string argv[])
 {
 //get the input from user
 do
-string plaintext = get_string("caesar key:");
-while (argc != 1||)
+int cipher_n = get_string("caesar key:");
+while (argc != 1||argv)
 //process the input
 string ciphertext = cipher_input(plaintext);
 //print the outcome in a proper way
