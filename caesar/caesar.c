ddaacf3e2c114333e69e0830f6f92ffbc1359a6b
penny5283 <74210456+penny5283@users.noreply.github.com>
2023-10-03 03:26:48 +0000

    Mon, Oct 2, 2023, 8:26 PM -07:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,9 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
+string cipher_input(string plaintext);
+
 int main(int argc, string argv[])
 {
 //get the input from user
 string plaintext = get_string("Plaintext:");
-//
+//process the input
+string ciphertext = cipher_input(plaintext);
+//print the outcome in a proper way
+
+}
+int cipher_input(string plaintext)
+{
+    
 }
