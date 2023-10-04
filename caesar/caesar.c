0b514c56c499d8365ac17b2bd325044056e5f527
penny5283 <74210456+penny5283@users.noreply.github.com>
2023-10-04 20:58:26 +0000

    Wed, Oct 4, 2023, 1:58 PM -07:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -2,29 +2,44 @@
 #include <stdio.h>
 
 string cipher_input(string plaintext);
-
+int key = 0;
 int main(int argc, string argv[])
 {
-//get the input from user
-do
-{
-    printf("caesar key: ");
-    string plaintext = get_string("")
-}
-while (cipher_n <= 0||);
+// Check if the string contains only digits or is a valid number
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (!isdigit(str[i]) && str[i] != '-' && str[i] != '+') {
+            printf("Usage: ./caesar key")
+            return 1; // Not a number
+        }
+    }
+    int key = argc;
+    return 0; // It's a number
+//get string input from user
+    string plaintext = get_string("plaintext: ");
 //process the input
-string ciphertext = cipher_input(plaintext);
+    string ciphertext = rotate(plaintext);
 //print the outcome in a proper way
-
+    printf("ciphertext: %c", ciphertext);
 }
-int cipher_input(string plaintext)
+string rotate(string plaintext)
 {
     string outcome = 0;
     char alpha = 0;
     for (int i = 0, n = strlen(plaintext); i < n; i++)
     {
         char alpha = plaintext[i];
-        outcome += alpha + cipher_n;
+        char post_char = alpha + key;
+        if (post_char >= 'a' && post_char <= 'z')
+        {
+            //rotate lowercase letters
+            post_char = 'a' + (post_char - 'a' + 1) % 26;
+        }
+        else if (post_char >= 'A' && post_char <= 'Z')
+        {
+            //rotate uppercase letters
+            post_char = 'A' + (post_char - 'A' + 1) % 26;
+        }
+        outcome += post_char;
     }
     return outcome;
 }
