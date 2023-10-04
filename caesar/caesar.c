2f3dda2f5ec8f9388ceb0a49f4b911f3fcf8f9a4
penny5283 <74210456+penny5283@users.noreply.github.com>
2023-10-04 21:13:34 +0000

    Wed, Oct 4, 2023, 2:13 PM -07:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,45 +1,59 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
 
-string cipher_input(string plaintext);
-int key = 0;
+string rotate(string plaintext, int key);
+
 int main(int argc, string argv[])
 {
-// Check if the string contains only digits or is a valid number
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (!isdigit(str[i]) && str[i] != '-' && str[i] != '+') {
-            printf("Usage: ./caesar key")
-            return 1; // Not a number
-        }
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
     }
-    int key = argc;
-    return 0; // It's a number
-//get string input from user
+
+    int key = atoi(argv[1]);
+
+    if (key <= 0)
+    {
+        printf("Key must be a positive integer.\n");
+        return 1;
+    }
+
     string plaintext = get_string("plaintext: ");
-//process the input
-    string ciphertext = rotate(plaintext);
-//print the outcome in a proper way
-    printf("ciphertext: %c", ciphertext);
+    string ciphertext = rotate(plaintext, key);
+
+    printf("ciphertext: %s\n", ciphertext);
+
+    return 0;
 }
-string rotate(string plaintext)
+
+string rotate(string plaintext, int key)
 {
-    string outcome = 0;
-    char alpha = 0;
+    string outcome = plaintext;
+
     for (int i = 0, n = strlen(plaintext); i < n; i++)
     {
         char alpha = plaintext[i];
-        char post_char = alpha + key;
-        if (post_char >= 'a' && post_char <= 'z')
+
+        if (isalpha(alpha))
         {
-            //rotate lowercase letters
-            post_char = 'a' + (post_char - 'a' + 1) % 26;
+            char post_char = alpha + key;
+
+            if (islower(alpha))
+            {
+                // Rotate lowercase letters
+                post_char = 'a' + (post_char - 'a') % 26;
+            }
+            else if (isupper(alpha))
+            {
+                // Rotate uppercase letters
+                post_char = 'A' + (post_char - 'A') % 26;
+            }
+
+            outcome[i] = post_char;
         }
-        else if (post_char >= 'A' && post_char <= 'Z')
-        {
-            //rotate uppercase letters
-            post_char = 'A' + (post_char - 'A' + 1) % 26;
-        }
-        outcome += post_char;
     }
     return outcome;
 }
