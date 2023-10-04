5f3d62cfc9353a22d8dfcdb981dc17d946606c21
penny5283 <74210456+penny5283@users.noreply.github.com>
2023-10-04 23:05:31 +0000

    Wed, Oct 4, 2023, 4:05 PM -07:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -116,6 +116,7 @@ string get_guess(int wordsize)
     // ensure users actually provide a guess that is the correct length
     // TODO #3
     guess = get_string("Input a 5-letter word:")
+    do
     
     return guess;
 }
