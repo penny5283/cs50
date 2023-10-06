8e0556cf1af51f3669fb86666ef6d1a845363276
penny5283 <74210456+penny5283@users.noreply.github.com>
2023-10-06 05:23:29 +0000

    Thu, Oct 5, 2023, 10:23 PM -07:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -115,7 +115,7 @@ string get_guess(int wordsize)
 
     // ensure users actually provide a guess that is the correct length
     // TODO #3
-    guess = get_string("Input a 5-letter word:")
+    guess = get_string("Input a 5-letter word:");
     do
     {
         printf ("Input a 5-letter word:");
--- wordle/wordle1.c
+++ wordle/wordle1.c
@@ -117,7 +117,12 @@ string get_guess(int wordsize)
 
     // ensure users actually provide a guess that is the correct length
     // TODO #3
-
+    guess = get_string("Input a 5-letter word:");
+    do
+    {
+        printf ("Input a 5-letter word:");
+    }
+    while (strlen(guess) !=wordsize);
     return guess;
 }
 
