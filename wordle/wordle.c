d266d0dc098bc9c1e494f39d6755fb8f53ee2af3
penny5283 <74210456+penny5283@users.noreply.github.com>
2023-10-06 05:15:22 +0000

    Thu, Oct 5, 2023, 10:15 PM -07:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -44,8 +44,6 @@ int main(int argc, string argv[])
 
     printf("You have 6 tries to guess the %d-letter word I'm thinking of\n", wordsize);
 
-    return 0;
-
     // open correct file, each file has exactly LISTSIZE words
     char wl_filename[6];
     sprintf(wl_filename, "%i.txt", wordsize);
