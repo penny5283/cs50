0430724796b57833665c232ca01962b30d14abfc
penny5283 <74210456+penny5283@users.noreply.github.com>
2023-10-06 05:41:08 +0000

    Thu, Oct 5, 2023, 10:41 PM -07:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -87,7 +87,8 @@ int main(int argc, string argv[])
 
         // set all elements of status array initially to 0, aka WRONG
         // TODO #4
-        for (int i = 0, i < strlen(wordsize))
+        for (int i = 0, i < wordsize; i++)
+        
         // Calculate score for the guess
         int score = check_word(guess, wordsize, status, choice);
 
