e0e99f099bb1fd5ec71bccf5c01e9d294e58b489
penny5283 <74210456+penny5283@users.noreply.github.com>
2023-10-08 23:54:08 +0000

    Sun, Oct 8, 2023, 4:54 PM -07:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -69,7 +69,10 @@ bool vote(string name)
     // TODO
     for(int i = 0; i < candidate_count; i++)
     {
-        if(strcmp() )
+        if(strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes ++;
+        }
     }
     return false;
 }
