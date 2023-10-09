726d5b09c3a9d49746f227251f147388defe5d6e
penny5283 <74210456+penny5283@users.noreply.github.com>
2023-10-09 02:04:47 +0000

    Sun, Oct 8, 2023, 7:04 PM -07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -115,6 +115,10 @@ bool vote(int rank, string name, int ranks[])
 void record_preferences(int ranks[])
 {
     // TODO
+    for(int i = 0; i < candidate_count; i++)
+    {
+        
+    }
     return;
 }
 
