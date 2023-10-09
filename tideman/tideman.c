efa3c39b95fcf6caa900dc4aeb399cc1f5993e45
penny5283 <74210456+penny5283@users.noreply.github.com>
2023-10-09 01:39:21 +0000

    Sun, Oct 8, 2023, 6:39 PM -07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -100,7 +100,10 @@ int main(int argc, string argv[])
 bool vote(int rank, string name, int ranks[])
 {
     // TODO
-    for(int i = 0; i < )
+    for(int i = 0; i < voter_count; i++)
+    {
+        
+    }
     return false;
 }
 
