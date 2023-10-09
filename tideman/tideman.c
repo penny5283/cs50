9e4fb389373a482e08037e422ecbef9a631a8306
penny5283 <74210456+penny5283@users.noreply.github.com>
2023-10-09 19:09:42 +0000

    Mon, Oct 9, 2023, 12:09 PM -07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -133,13 +133,22 @@ void record_preferences(int ranks[])
 void add_pairs(void)
 {
     // TODO
+    pair_count = 0;
     for(int i = 0; i < candidate_count; i++)
     {
         for (int j = i + 1; j < candidate_count; j++)
         {
-            if(preferences[i][j]>0)
+            if(preferences[i][j] > preferences[j][i])
             {
-                
+                pairs[pair_count].winner = i;
+                pairs[pair_count].loser = j;
+                pair_count++;
+            }
+            else if(preferences[i][j] < preferences[j][i])
+            {
+                pairs[pair_count].winner = j;
+                pairs[pair_count].loser = i;
+                pair_count++;
             }
         }
     }
