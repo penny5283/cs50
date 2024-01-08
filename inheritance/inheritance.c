ab00b44b8a7c3d2baea680503ea21f9f70e10ec3
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-08 03:45:43 +0000

    Sun, Jan 7, 2024, 7:45 PM -08:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -54,8 +54,8 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        new_person -> parent[0] = parent[0];
-        new_person -> parent[1] = parent[1];
+        new_person -> parents[0] = parent[0];
+        new_person -> parents[1] = parent[1];
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
     int random_index_for_parent0 = rand() % 2;
     int random_index_for_parent1 = rand() % 2;
