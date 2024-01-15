99d71b1c947dc0cdb34911fc7ac8badab962be74
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-15 04:41:55 +0000

    Sun, Jan 14, 2024, 8:41 PM -08:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -58,6 +58,8 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
+    winners = simulate_round(teams)
+    if l
 
 
 if __name__ == "__main__":
