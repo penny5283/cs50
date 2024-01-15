f24355d82ec779dfd4e3de7a50d131a41a56018b
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-15 05:29:04 +0000

    Sun, Jan 14, 2024, 9:29 PM -08:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -26,8 +26,6 @@ def main():
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
-    w_count = 0
-
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
