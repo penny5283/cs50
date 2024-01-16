8a9b2e70258773c643870d9d05ced6ddf835320a
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-16 23:15:05 +0000

    Tue, Jan 16, 2024, 3:15 PM -08:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -3,14 +3,18 @@ from cs50 import get_string
 # Get user input
 txt = get_string("Text: ")
 
+
 def count_letters(text):
     return sum(1 for char in text if char.isalpha())
+
 
 def count_words(text):
     return len(text.split())
 
+
 def count_sentences(text):
     return sum(1 for char in text if char in ['.', '?', '!'])
+
 
 count_l = count_letters(txt)
 count_w = count_words(txt)
@@ -33,6 +37,3 @@ elif formatted_index >= 16:
     print("Grade 16+")
 else:
     print(f"Grade {formatted_index}")
-
-
-
