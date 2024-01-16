2323e5b8377babae4d639fcf5abe58ff0e73fa60
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-16 04:29:17 +0000

    Mon, Jan 15, 2024, 8:29 PM -08:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -5,7 +5,5 @@ while h <= 0 or h > 8:
     h = get_int("Height:")
 for i in range(1, h+1):
     for j in range(h-i):
-        print(" ", end = "")
-    for p in range(i):
         print("#")
 print()
