4687f5091ed9c092ebf474fde08902444337497b
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-16 22:15:32 +0000

    Tue, Jan 16, 2024, 2:15 PM -08:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -4,8 +4,13 @@ txt = get_string("Text: ")
 def check_letters(text):
     count_l = 0
     for i in text:
-        i = text[]
         if i.isalpha():
             count_l += 1
+    return count_l
+def check_words(text):
+    count_w = 0
+    pre = None
+    for i in text:
+        if 
 
 
