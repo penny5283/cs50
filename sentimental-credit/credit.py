a4c6ecd3863d1f657c7bc1229b91b83dbac0d2a3
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-16 06:43:50 +0000

    Mon, Jan 15, 2024, 10:43 PM -08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,35 +1,42 @@
-# TODO
 import re
 from cs50 import get_string
+
 amex_regex = r"^3(4|7)[0-9]{13}$"
 mastercard_regex = r"^(5[1-5][0-9]{14}|2[3-6][0-9]{14}|27[0-1][0-9]{14}|2720[0-9]{12})$"
-visa_regex = visa_regex = r"^4[0-9]{12}([0-9]{3})?$"
-input = get_string("Number:")
-def check_luhn(card_number):
+visa_regex = r"^4[0-9]{12}([0-9]{3})?$"
+input_number = get_string("Number:")
 
+def check_luhn(card_number):
     def digits_of(n):
-        return [int(digit) for digit in n]
+        return [int(i) for i in str(n)]  # Convert each digit to int
     digits = digits_of(card_number)
+    total_sum = 0
+    # Process odd and even digits
     odd_digits = digits[-2::-2]
     even_digits = digits[-1::-2]
-    total_sum = 0
-    for i in odd_digits:
-        doubled = i * 2
+
+    for digit in odd_digits:
+        doubled = digit * 2
         if doubled >= 10:
             total_sum += sum(digits_of(doubled))
         else:
-            total_sum += i
-    for i in even_digits:
-        total_sum += i
-    return sum[-1]
-if re.match(input, amex_regex):
+            total_sum += doubled
+
+    for digit in even_digits:
+        total_sum += digit
+
+    return total_sum % 10 == 0
+
+# Matching card type and validating with Luhn's algorithm
+if re.match(amex_regex, input_number):
     print("AMEX")
-elif re.match(input, mastercard_regex):
+elif re.match(mastercard_regex, input_number):
     print("MASTERCARD")
-elif re.match(input, visa_regex):
-    c = check_luhn(input)
-    if c == 0:
+elif re.match(visa_regex, input_number):
+    if check_luhn(input_number):
         print("VISA")
+    else:
+        print("INVALID")
 else:
     print("INVALID")
 
