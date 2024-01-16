4c755a1346f7d1b2b442510ad4b98312ea1f2cd6
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-16 06:10:43 +0000

    Mon, Jan 15, 2024, 10:10 PM -08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,14 +1,16 @@
 # TODO
 import re
-from cs50 import get_int
+from cs50 import get_string
 amex_regex = r"^3(4|7)[0-9]{13}$"
 mastercard_regex = r"^(5[1-5][0-9]{14}|2[3-6][0-9]{14}|27[0-1][0-9]{14}|2720[0-9]{12})$"
 visa_regex = visa_regex = r"^4[0-9]{12}([0-9]{3})?$"
-input = get_int("Number:")
+input = get_string("Number:")
 def check_luhn(card_number):
     def digits_of(n):
-        return int
-
+        for i in n():
+            return int(i)
+    digit = digits_of(card_number)
+    odd_digit = 
 if re.match(input, amex_regex):
     print("AMEX")
 if re.match(input, mastercard_regex):
