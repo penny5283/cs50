2994bee1f23d4a72659ec7b8edf6a00aa615a380
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-16 04:16:59 +0000

    Mon, Jan 15, 2024, 8:16 PM -08:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -3,9 +3,9 @@ from cs50 import get_int
 h = get_int("Height:")
 while h <= 0 or h > 8:
     h = get_int("Height:")
-for i in range(h):
+for i in range(1, h):
     for j in range(h-i):
-        print("")
+        print(" ")
     for p in range(1,h+1):
         print("#" + "  " + "#", end = "")
 print()
