44c51cea880df3d30dea34a5c385cd613dccacc4
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-16 22:36:58 +0000

    Tue, Jan 16, 2024, 2:36 PM -08:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -31,14 +31,18 @@ count_l = check_letters(txt)
 count_w = check_words(txt)
 count_s = check_sentences(txt)
 
-def cal_index(text):
-    index = 0
-    L = count_l / count_w * 100
-    S = count_s / count_w * 100
-    index = 0.0588 * L - 0.296 * S - 15.8
 
+index = 0
+L = count_l / count_w * 100
+S = count_s / count_w * 100
+index = 0.0588 * L - 0.296 * S - 15.8
 
-
+if index < 1:
+    print("Before Grade 1")
+elif index >= 16:
+    print("Grade 16+")
+else:
+    print("Grade {}".format(index))
 
 
 
