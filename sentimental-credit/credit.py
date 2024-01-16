89d3bd16552c44bb64f86e7fcb234dcae98b9ceb
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-16 05:42:36 +0000

    Mon, Jan 15, 2024, 9:42 PM -08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,8 +1,3 @@
 # TODO
 import re
-amex_regex = ^3(4|7)[0-9]{13}$
-card_number = "371234567890123"  # Example Amex card number
-if re.match(amex_regex, card_number):
-    print("Valid American Express card number")
-else:
-    print("Invalid American Express card number")
+amex_regex = r"^3(4|7)[0-9]{13}$"
