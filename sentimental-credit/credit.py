c5823d270a12c7d839c3e0e0487aba9a4946dfeb
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-16 06:33:15 +0000

    Mon, Jan 15, 2024, 10:33 PM -08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -6,9 +6,9 @@ mastercard_regex = r"^(5[1-5][0-9]{14}|2[3-6][0-9]{14}|27[0-1][0-9]{14}|2720[0-9
 visa_regex = visa_regex = r"^4[0-9]{12}([0-9]{3})?$"
 input = get_string("Number:")
 def check_luhn(card_number):
+
     def digits_of(n):
-        for i in n:
-            return int(i)
+        return [int(digit) for digit in n]
     digits = digits_of(card_number)
     odd_digits = digits[-2::-2]
     even_digits = digits[-1::-2]
