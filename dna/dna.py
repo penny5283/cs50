0086fa0958421dc58a537e7bc5a69e3b51ab2d56
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-17 20:09:22 +0000

    Wed, Jan 17, 2024, 12:09 PM -08:00

--- dna/dna.py
+++ dna/dna.py
@@ -27,7 +27,7 @@ def main():
         gene_str = "".join(gene)
         print(gene_str)
     # TODO: Find longest match of each STR in DNA sequence
-    d
+    
     # TODO: Check database for matching profiles
 
     return
