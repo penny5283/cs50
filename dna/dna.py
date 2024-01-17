3c90f8d5f08c5657ff02f68418a46b7d7cb8bedd
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-17 23:11:07 +0000

    Wed, Jan 17, 2024, 3:11 PM -08:00

--- dna/dna.py
+++ dna/dna.py
@@ -26,9 +26,9 @@ def main():
     # TODO: Find longest match of each STR in DNA sequence
     longest_runs = {}
     longest_run = 0
-    for pattern in str_patterns:
-        longest_run = longest_match(gene_str, pattern)
-        longest_runs[pattern] = longest_run
+    for profile in profiles:
+        longest_run = longest_match(gene_str, profile)
+        longest_runs[profile] = longest_run
     # TODO: Check database for matching profiles
 
     return
