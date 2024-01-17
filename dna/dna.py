fa56d800257ad6657a3eca62408b71004d836691
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-17 00:09:30 +0000

    Tue, Jan 16, 2024, 4:09 PM -08:00

--- dna/dna.py
+++ dna/dna.py
@@ -8,7 +8,7 @@ def main():
     if len(sys.argv) != 4:
         print("python dna.py data.csv sequence.txt")
     # TODO: Read database file into a variable
-    
+    with open()
     # TODO: Read DNA sequence file into a variable
 
     # TODO: Find longest match of each STR in DNA sequence
