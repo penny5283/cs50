bb79b3b3523e736de6f1c9cbb374ec91d6ceec2e
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-18 23:06:47 +0000

    Thu, Jan 18, 2024, 3:06 PM -08:00

--- seven-day-average/seven-day-average.py
+++ seven-day-average/seven-day-average.py
@@ -34,7 +34,8 @@ def main():
 
 # TODO: Create a dictionary to store 14 most recent days of new cases by state
 def calculate(reader):
-    ...
+    new_cases = {}
+    previous_cased
 
 
 # TODO: Calculate and print out seven day average for given state
