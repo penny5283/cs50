391f8a935af6cee8ec605a4f9c6069f9f06176d5
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-18 09:14:29 +0000

    Thu, Jan 18, 2024, 1:14 AM -08:00

--- jar/jar.py
+++ jar/jar.py
@@ -2,7 +2,7 @@ class Jar:
     def __init__(self, capacity=12):
         self.capacity  = capacity
         if self.capacity < 0:
-            
+            raise ValueError
     def __str__(self):
         ...
 
