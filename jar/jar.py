45cddb1c4e7904677e91169d4f9d54e5b674de57
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-18 20:00:21 +0000

    Thu, Jan 18, 2024, 12:00 PM -08:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,21 +1,32 @@
 class Jar:
     def __init__(self, capacity=12):
         self.capacity  = capacity
+        self.cookies = 0
         if self.capacity < 0:
             raise ValueError("Capacity cannot be negative")
-    def __str__(self):
-        ...
 
+    def __str__(self):
+        return "🍪" * self.cookies
     def deposit(self, n):
-        ...
+        if self.cookies + n > self.capacity:
+            raise ValueError("Exceed capacity")
+        else:
+            self.cookies += n
+            return self.cookies
 
     def withdraw(self, n):
-        ...
+        if n - self.cookies >= 0:
+            self.cookies -= n
+        else:
+            raise ValueError("Do not have enough cookies")
 
     @property
     def capacity(self):
-        ...
+        return self.capacity - self.cookies
 
     @property
     def size(self):
-        ...
+        return self.cookies
+
+    def main():
+        
