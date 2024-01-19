2dd632e52071efa180d4f43a765a19ce90fb6964
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-19 04:38:19 +0000

    Thu, Jan 18, 2024, 8:38 PM -08:00

--- seven-day-average/seven-day-average.py
+++ seven-day-average/seven-day-average.py
@@ -9,7 +9,7 @@ def main():
     )
     decoded_content = download.content.decode("utf-8")
     print(type(decoded_content))
-    #reader = pd.read_csv(file)
+    reader = pd.read_csv(decoded_content)
 
     # Construct 14 day lists of new cases for each states
     new_cases = calculate(reader)
