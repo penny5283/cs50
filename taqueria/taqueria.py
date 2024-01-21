7a56fb7bff2807bed542cea5fafc57e035ec2aae
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-21 00:09:28 +0000

    Sat, Jan 20, 2024, 4:09 PM -08:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -9,16 +9,20 @@ menu = {
     "Taco": 3.00,
     "Tortilla Salad": 8.00
 }
+
 def main():
-    total_price = 0
-    try:
-        item = (input("Item:\n")).title()
-        for key in menu:
-            if key == item:
-                total_price += int(menu[key])
-            else:
-                print("Not in the menu")
-        print(f"Total: ${total_price:.2f}")
-    except EOFerror:
+    total_price = 0.0
+    while True:
+        try:
+            item = input("Item: ").title()
+            if item in menu:
+                total_price += menu[item]
+                print(f"Total: ${total_price:.2f}")
+            # No need to notify the user if the item is not in the menu
+        except EOFError:
+            print("\n")  # Move the cursor to a new line
+            break
+
+    print(f"Final Total: ${total_price:.2f}")
 
 main()
