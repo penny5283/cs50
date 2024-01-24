546ad044cacad1fff7cd2526088f36c193196417
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-24 06:38:12 +0000

    Tue, Jan 23, 2024, 10:38 PM -08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -24,4 +24,4 @@ SELECT id, account_number, amount FROM atm_transactions
 WHERE year = 2021 AND month = 7 AND day = 28
 AND atm_location = 'Leggett Street'
 AND transaction_type = 'withdraw';
-SELECT 
+SELECT id, 
