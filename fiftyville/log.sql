80461a746a267e23512c868ba721f91cc99d9950
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-24 06:05:32 +0000

    Tue, Jan 23, 2024, 10:05 PM -08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -5,8 +5,9 @@
 SELECT * FROM crime_scene_reports;
 --it seems that description include the type of crime committed
 --filter all the the theft happened on july 28,2021 and on that place
-SELECT id FROM crime_scene_reports
+SELECT id, description FROM crime_scene_reports
 WHERE year = 2021 AND month = 7 AND day = 28
 AND street = 'Humphrey Street'
-AND description LIKE 'Theft%'
-
+AND description LIKE 'Theft%';
+--get the id->295
+--
