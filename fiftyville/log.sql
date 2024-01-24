9f178ea4c0e0989656358f4016c1bd9baa1f13c5
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-24 07:20:17 +0000

    Tue, Jan 23, 2024, 11:20 PM -08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -67,4 +67,4 @@ AND bank_accounts.account_number IN (SELECT account_number FROM atm_transactions
 WHERE year = 2021 AND month = 7 AND day = 28
 AND atm_location = 'Leggett Street'
 AND transaction_type = 'withdraw')
-AND ;
+AND flights.id = 36;
