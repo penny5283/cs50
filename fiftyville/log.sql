44a0f906de55f458f3b372a98eabf456b8b0b3ce
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-24 06:11:11 +0000

    Tue, Jan 23, 2024, 10:11 PM -08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,15 +1,16 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 -- first using schema
 .schema
---what kind of description it provided
+-- what kind of description it provided
 SELECT * FROM crime_scene_reports;
---it seems that description include the type of crime committed
---filter all the the theft happened on july 28,2021 and on that place
+-- it seems that description include the type of crime committed
+-- filter all the the theft happened on july 28,2021 and on that place
 SELECT id, description FROM crime_scene_reports
 WHERE year = 2021 AND month = 7 AND day = 28
 AND street = 'Humphrey Street'
 AND description LIKE 'Theft%';
---get the id->295
---time->10:15am
---place->Humphrey Street bakery
-
+-- get the id->295
+-- time->10:15am
+-- place->Humphrey Street bakery
+-- 3 witnesses mentioned the bakery
+--
