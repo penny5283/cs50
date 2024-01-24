f1eab99a2e25bbe642d32f69624706936cfe76aa
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-24 01:53:28 +0000

    Tue, Jan 23, 2024, 5:53 PM -08:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,4 +1,4 @@
-SELECT movies.title, ratings.rating FROM movies
+SELECT movies.title, FROM movies
 JOIN ratings ON movies.id = ratings.movie_id
 WHERE movies.year = 2010 AND ratings.rating != NULL
 ORDER BY ratings.rating DESC, movies.title ASC;
