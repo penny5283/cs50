06970f52d99bb83b031d96a9a5ee6822e2777e7b
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-24 06:43:37 +0000

    Tue, Jan 23, 2024, 10:43 PM -08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -26,6 +26,6 @@ AND atm_location = 'Leggett Street'
 AND transaction_type = 'withdraw';
 SELECT id, license_plate FROM bakery_security_logs
 WHERE year = 2021 AND month = 7 AND day = 28
-AND hour = 10 AND minute > 15 AND minute <= 25
+AND hour = 10 AND minute > 15 AND minute < 25
 AND activity = 'exit';
 
