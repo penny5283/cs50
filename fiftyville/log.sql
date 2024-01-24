777fdf2fd2f3900ad9185b396e83b2cb8e5c2f1a
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-24 05:46:11 +0000

    Tue, Jan 23, 2024, 9:46 PM -08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
-##
+-- first using schema
+.schema
+--what kind of description it provided
+SELECT * FROM crime_scene_reports
+
+
