cbf034fc00e5763e83e9abf3acac708010c251d8
Jing Ding <74210456+penny5283@users.noreply.github.com>
2024-01-24 01:16:03 +0000

    Tue, Jan 23, 2024, 5:16 PM -08:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,1 +1,9 @@
-
+SELECT movies.title FROM movies
+JOIN stars ON movies.id = stars.movie_id
+JOIN people ON stars.person_id = people.id
+WHERE people.name = 'Jennifer Lawrence'
+AND movies.id IN
+(SELECT movies.id FROM movies
+JOIN stars ON movies.id = stars.movie_id
+JOIN people ON stars.per_id = people.id
+WHERE people.name = 'Bradley Cooper');
